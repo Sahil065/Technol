c4bf25c64d33360179efa20ad18983348d6e111a
Sahil <sahilsaini1969@gmail.com>
2019-11-24 22:20:12 +0530

    images path updated

--- js/technol.js
+++ js/technol.js
@@ -70,5 +70,5 @@ $(document).ready(function() {
 });
 
 function setCoverImage(){
-  $(".intro-container").css("background-image", "url('images/cover/" + coverImage + "')");  
+  $(".intro-container").css("background-image", "url('images/Cover/" + coverImage + "')");  
 }
